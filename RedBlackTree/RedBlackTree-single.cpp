252d54bb76e852dc813a9d41436f5aedbe5495d1
nimjk <nimjk@naver.com>
2023-08-21 15:37:39 +0900

    레드블랙트리 - 싱글스레드코드 완성

--- RedBlackTree/RedBlackTree-single.cpp
+++ RedBlackTree/RedBlackTree-single.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int threadcount;
 
 mt19937 engine(GetTickCount());                    // MT19937 난수 엔진
-uniform_int_distribution<int> distribution(1, 90000);
+uniform_int_distribution<int> distribution(1, 1000000);
 
 RedBlackTree<int> rbTree;
 
@@ -25,7 +25,7 @@ int main() {
     double duration;
 
     start = clock();
-    while(count < 50000){
+    while(count < 400000){
         int tmp = distribution(engine);
         int isSame = 0;
         for (int i = 0; i < treeData.size(); i++){
@@ -45,74 +45,95 @@ int main() {
 
     duration = (double)(end - start) / CLOCKS_PER_SEC;
 
-    cout << "Red-Black Tree:" << endl;
-    rbTree.printTree(rbTree.getRoot());
-    cout << "Time of generation tree : " << duration << "초\n";
-
     int searchcnt = 0;
 
-
+    clock_t searchstart, searchend;
+    double searchduration;
     double mintime, maxtime;
     double totaltime = 0;
     double meantime = 0;
 
 
-    while(searchcnt < 20000){
-        uniform_int_distribution<int> searchdistribution(0, 49999);
+    while(searchcnt < 100000){
+        uniform_int_distribution<int> searchdistribution(0, 399999);
         int searchidx = searchdistribution(engine);
         int searchnum = distribution(engine);
 
-        start = clock();
+        searchstart = clock();
         rbTree.search(searchnum); // 배열 index로 할꺼면 searchnum 대신에 treeData[searchidx]
-        end = clock();
+        searchend = clock();
 
-        duration = (double)(end - start) / CLOCKS_PER_SEC;
+        searchduration = (double)(searchend - searchstart) / CLOCKS_PER_SEC;
 
         if(searchcnt == 0){
-            mintime = duration;
-            maxtime = duration;
+            mintime = searchduration;
+            maxtime = searchduration;
         }
 
         else{
-            if(mintime > duration){
-                mintime = duration;
+            if(mintime > searchduration){
+                mintime = searchduration;
             }
-            else if(maxtime < duration){
-                maxtime = duration;
+            else if(maxtime < searchduration){
+                maxtime = searchduration;
             }
         }
 
-        totaltime += duration;
+        totaltime += searchduration;
 
         searchcnt++;
     }
 
     meantime = totaltime / (searchcnt+1);
 
+    // Single Thread Process
+    clock_t delstart, delend, insstart, insend;
+    int delrun = 0;
+    int insertrun = 0;
+    double delruntime = 0;
+    double insertruntime = 0;
+
+    while (delrun<200000){
+
+        int deltmp = distribution(engine);
+        delstart = clock();
+
+        rbTree.erase(deltmp);
+
+        delend = clock();
+
+        delruntime += (double)(delend-delstart)/CLOCKS_PER_SEC;
+
+        delrun++;
+    }
+    delend = clock();
+
+    
+    while (insertrun<200000){
+        uniform_int_distribution<int> insertdistribution(1000001, 2000000);
+        int instmp = insertdistribution(engine);
+        insstart = clock();
+        rbTree.insert(instmp);
+        insend = clock();
+
+        insertruntime += (double)(insend-insstart)/CLOCKS_PER_SEC;
+
+        insertrun++;
+    }
+
+
+
+    cout << "Red-Black Tree:" << endl;
+    rbTree.printTree(rbTree.getRoot());
+    cout << "Time of generation tree : " << duration << "초\n";
+
     cout << "minimum time of search : " <<fixed<< mintime << "초\n";
     cout << "maximum time of search : " << maxtime << "초\n";
     cout << "total time of search : " << totaltime << "초\n";
     cout << "average time of search : " << meantime << "초\n";
 
-    // Single Thread Process
-    clock_t start1, end1;
-    int singlethreadrun = 0;
-    double singleruntime;
-    start1 = clock();
-    while (singlethreadrun<30000){
-        int deltmp = distribution(engine);
-        int instmp = distribution(engine)+100000;
-
-        rbTree.erase(deltmp);
-        rbTree.insert(instmp);
-
-        singlethreadrun++;
-    }
-    end1 = clock();
-
-    singleruntime = (double)(end1-start1)/CLOCKS_PER_SEC;
-
-    cout << fixed << singleruntime;    
+    cout << delruntime << "초\n";    
+    cout << insertruntime<< "초";
 
     return 0;
 }
